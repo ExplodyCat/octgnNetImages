4795e448edf9985fbba2eb608f42cf29216a4958
Jerry <JeremyStormo@gmail.com>
2014-01-24 18:39:57 -0600

    Release!

--- README.txt
+++ README.txt
@@ -4,12 +4,6 @@ Downloads uncensored card images to be used by the application.
 
 Features:
   Tries multiple sites before fail
-  No updates required to dl new set releases
-
-Current Goals:
-  Only download image if theres a higher rez one available
-  More failback sites
-  Multithreaded
-  Reuse connection for multiple downloads
-  Rewrite into Go (for learning/multiprocessing)
+  Only download image if theres a higher quality one available
+  
   
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io/ioutil"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,8 +13,8 @@ import (
 	"path"
 	"sync"
 	//"sort"
-	"strings"
 	"encoding/xml"
+	"strings"
 )
 
 type CardInfo struct {
@@ -30,6 +30,7 @@ type AssetSource struct {
 	ComposeURL func(info CardInfo) string
 }
 type AssetList []AssetSource
+
 var sources AssetList = AssetList{
 	{
 		Quality: 125400,
@@ -57,11 +58,12 @@ var sources AssetList = AssetList{
 var pngSig []byte = []byte{'\x89', '\x50', '\x4E', '\x47', '\x0D', '\x0A', '\x1A', '\x0A'}
 
 const (
-	octgnGameId string = "0f38e453-26df-4c04-9d67-6d43de939c77"
-	markerSetId string = "21bf9e05-fb23-4b1d-b89a-398f671f5999"
-	consumeThreads int = 1
-	chanSize int = 60
+	octgnGameId    string = "0f38e453-26df-4c04-9d67-6d43de939c77"
+	markerSetId    string = "21bf9e05-fb23-4b1d-b89a-398f671f5999"
+	consumeThreads int    = 1
+	chanSize       int    = 60
 )
+
 var wGroup sync.WaitGroup
 var wChan = make(chan Task, chanSize)
 
@@ -96,9 +98,9 @@ func parseSetXML(xmlPath string) (results []CardInfo, err error) {
 	v := new(NetSet)
 	err = xml.Unmarshal(data, &v)
 	if err != nil {
-		return results,err
+		return results, err
 	}
-	
+
 	//parse out all the cards
 	for _, cur := range v.Cards.Cards {
 		newItem := CardInfo{
@@ -128,112 +130,114 @@ func getPaths() (setPath string, imgPath string) {
 func consumer() {
 	defer wGroup.Done()
 	for curTask := range wChan {
-		for _,curAsset := range sources{
+		for _, curAsset := range sources {
 			//Cycle through sources so long as resolution quality of source is better than
 			//current card's imgQuality.  No card == quality 0.
-			
-			if curTask.Card.Quality >= curAsset.Quality{
+
+			if curTask.Card.Quality >= curAsset.Quality {
 				//No more good sources, abort current task
 				break
 			}
-			
+
 			url := curAsset.ComposeURL(curTask.Card)
-			fmt.Printf("Attempting download: %s - %s\n",curTask.Card.Set,curTask.Card.Name)
-			if err := doDownload(url,curTask.Target); err == nil{
+			fmt.Printf("Attempting download: %s - %s\n", curTask.Card.Set, curTask.Card.Name)
+			if err := doDownload(url, curTask.Target); err == nil {
 				//Download was a success, can abort current task
 				break
-			} else{
+			} else {
 				//Download failed, keep looping for next source
-				fmt.Printf("Failed Get: %s - %s\n\t%s\n",curTask.Card.Set, curTask.Card.Name,err)
+				fmt.Printf("Failed Get: %s - %s\n\t%s\n", curTask.Card.Set, curTask.Card.Name, err)
 			}
 		}
 	}
 }
 
 //Download contents of url to target path
-func doDownload(url string, target string) (err error){
+func doDownload(url string, target string) (err error) {
 	//Get the url contents
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
-	if err !=nil{
+	if err != nil {
 		return err
 	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed to get url: HTTP:%d:%s\n\t%s\n", resp.StatusCode, http.StatusText(resp.StatusCode), url)
+	}
+
 	//Open file handle
-	target = "TMP"//DEBUG
 	out, err := os.Create(target)
 	defer out.Close()
-	if err !=nil{
+	if err != nil {
 		return err
 	}
-	fmt.Println(resp.Body)
+
 	//Copy response into file
 	_, err = io.Copy(out, resp.Body)
-	if err !=nil{
+	if err != nil {
 		return err
 	}
 	return
 }
 
 //Loads wChan with list of Netrunner cards to consider downloading
-func producer(){
+func producer() {
 	setPath, imgPath := getPaths()
-	
+
 	//get list of set directories
 	setDirs, err := ioutil.ReadDir(setPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	for _, curSet := range setDirs{
-	
+
+	for _, curSet := range setDirs {
+
 		//Skip over the marker set
 		if curSet.Name() == markerSetId {
 			continue
 		}
-		
+
 		//Get the collection of cards from the set
 		setFile := fmt.Sprintf("%s/%s/set.xml", setPath, curSet.Name())
 		setColl, err := parseSetXML(setFile)
-		if err != nil{
-			fmt.Printf("Error loading set file: %s\n\t%s\n",setFile, err)
+		if err != nil {
+			fmt.Printf("Error loading set file: %s\n\t%s\n", setFile, err)
 			continue
 		}
-		
-		for _, curCard:= range setColl{
-		
+
+		for _, curCard := range setColl {
+
 			//Get card quality
-			curPath := path.Join(imgPath, curCard.SetID, "Cards", curCard.ID+ ".png")
-			curCard.Quality = getPNGQuality(curPath)			
+			curPath := path.Join(imgPath, curCard.SetID, "Cards", curCard.ID+".png")
+			curCard.Quality = getPNGQuality(curPath)
 			//if best quality of source > card Quality, queue for downloads
-			if curCard.Quality < sources[0].Quality{			
+			if curCard.Quality < sources[0].Quality {
 				wChan <- Task{curPath, curCard}
 			}
 		}
 	}
 }
 
-func unpackUInt(bSlice []byte) (result uint32){
+func unpackUInt(bSlice []byte) (result uint32) {
 	//TODO: Endianness check??
 	binary.Read(bytes.NewBuffer(bSlice), binary.BigEndian, &result)
 	return
 }
 
-func getPNGQuality(curPath string) uint{
-	fHandle, err := os.Open(curPath)	
+func getPNGQuality(curPath string) uint {
+	fHandle, err := os.Open(curPath)
 	defer fHandle.Close()
 	if err != nil {
 		return 0
 	}
-	
-	
+
 	head := make([]byte, 24)
-	count, err := fHandle.Read(head)	
-	if err !=nil || count != len(head) || !bytes.Equal(head[0:8],pngSig){		
+	count, err := fHandle.Read(head)
+	if err != nil || count != len(head) || !bytes.Equal(head[0:8], pngSig) {
 		return 0
 	}
-	
+
 	quality := uint(unpackUInt(head[16:20]) * unpackUInt(head[20:24]))
-	return quality	
+	return quality
 }
 
 func main() {
@@ -241,7 +245,7 @@ func main() {
 		wGroup.Add(1)
 		go consumer()
 	}
-	producer()	
+	producer()
 	close(wChan)
 	wGroup.Wait()
 }
